107926d5e54791be936a449b5364759b42eb4499
addy1110 <addy.1110@gmail.com>
2017-01-10 00:24:57 -0500

    added Bubble Sort

---
+++ Sorting/src/BubbleSort.java
@@ -0,0 +1,36 @@
+/**
+ * Created by ADDY on 09/01/17.
+ * src: https://www.hackerrank.com/challenges/ctci-bubble-sort
+ * Time Complexity: O(n*n)
+ * Length of Array: 100000
+ * Execution Time: 5.45200 seconds
+ */
+
+import java.util.Scanner;
+class BubbleSort {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int lengthOfArray = in.nextInt();
+        int[] array = new int[lengthOfArray];
+        for(int i=0;i<lengthOfArray;i++){
+            array[i] = in.nextInt();
+        }
+        in.close();
+
+        int numberOfSwaps=0;
+        for (int i=0;i<lengthOfArray;i++){
+            for(int j=0;j<lengthOfArray-1-i;j++){
+                if (array[j] > array[j + 1]) {
+                    int temp=array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+        }
+
+        System.out.println("Array is sorted in "+numberOfSwaps+" swaps.");
+        System.out.println("First Element: "+array[0]);
+        System.out.println("Last Element: "+array[lengthOfArray-1]);
+    }
+}
