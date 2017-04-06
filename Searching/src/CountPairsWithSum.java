242d9f7feb069b3621dc9161c085b8689c866acb
addy1110 <addy.1110@gmail.com>
2017-04-06 17:07:43 -0400

    added Searching Module

---
+++ Searching/src/CountPairsWithSum.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ADDY on 06/04/17.
+ */
+public class CountPairsWithSum {
+
+    static int countPairs(int[] arr){
+        int count=-1;
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,9,8,7,6,10,11,12,13,19,18,17,16,15,14};
+
+        System.out.println(countPairs(arr));
+    }
+}
---
+++ Searching/src/FindDuplicates.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+/**
+ * Created by ADDY on 06/04/17.
+ */
+public class FindDuplicates {
+
+    static void printDuplicates(int[] arr){
+        Set<Integer> detectDuplicates = new HashSet<>();
+        HashSet<Integer> dups = new HashSet<>();
+
+        for(int item: arr){
+            if(!detectDuplicates.add(item)){
+                dups.add(item);
+            }
+        }
+
+        for(Integer duplicates: dups) System.out.print(duplicates+" ");
+    }
+
+    static void countDuplicates(int[] arr){
+
+        HashMap<Integer,Integer> repetitions = new HashMap<>();
+        for(int item: arr){
+            if(repetitions.containsKey(item)) repetitions.put(item, repetitions.get(item)+1);
+            else repetitions.put(item,1);
+        }
+
+        System.out.println(repetitions);
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,6,9,1,4,8,2,5,3,10,6,7,2,5};
+        //printDuplicates(arr);
+
+        TreeSet<Integer> treeset = new TreeSet<>();
+        TreeMap<Integer,String> treeMap = new TreeMap<>();
+        for(int item: arr){
+            treeset.add(item);
+            treeMap.put(item,"value");
+        }
+
+        //System.out.println(treeset);
+        //System.out.println(treeMap);
+
+        countDuplicates(arr);
+    }
+}
